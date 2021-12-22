43669af0f3d63104d3d4637183c81067b5bcbfb7
Manuel Schmid <manysch3@gmail.com>
2021-12-22 23:11:20 +0100

    implemented editable rows for student overview

--- Printer-Reservation-System/StudentsOverview.aspx
+++ Printer-Reservation-System/StudentsOverview.aspx
@@ -2,19 +2,11 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="PlaceHolderHeader" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="PlaceHolderMain" runat="server">
-
-    <asp:GridView ID="gridviewStudents" runat="server">
+    /* AutoGenerateColumns="false" DataKeyNames="id" */
+    <asp:GridView ID="gridviewStudents" runat="server" OnPageIndexChanging="gridviewStudents_PageIndexChanging" OnRowCancelingEdit="gridviewStudents_RowCancelingEdit" OnRowDeleting="gridviewStudents_RowDeleting" OnRowEditing="gridviewStudents_RowEditing" OnRowUpdating="gridviewStudents_RowUpdating">
         <Columns>  
-            <asp:BoundField DataField="Name" HeaderText="Name" />  
-            <asp:BoundField DataField="Vorname" HeaderText="Vorname" />  
-            <asp:BoundField DataField="E-Mail" HeaderText="E-Mail" />  
-            <asp:BoundField DataField="Handy" HeaderText="Handy" />  
-            <asp:BoundField DataField="Bemerkung" HeaderText="Bemerkung" />  
-            <asp:BoundField DataField="Status" HeaderText="Status" />  
-            <asp:BoundField DataField="Beschreibung" HeaderText="Beschreibung" />  
-            <asp:BoundField DataField="Admin" HeaderText="Admin" />  
             <asp:CommandField ShowEditButton="true" />  
-            <asp:CommandField ShowDeleteButton="true" />
+            <asp:CommandField ShowDeleteButton="true" /> 
         </Columns>
     </asp:GridView>
 
--- Printer-Reservation-System/StudentsOverview.aspx.cs
+++ Printer-Reservation-System/StudentsOverview.aspx.cs
@@ -12,21 +12,25 @@ namespace Printer_Reservation_System
     public partial class StudentsOverview : System.Web.UI.Page
     {
         readonly SqlConnectionStringBuilder conBuilder = new SqlConnectionStringBuilder();
+        SqlConnection con = new SqlConnection();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             conBuilder.DataSource = @"NOTEBOOKMANY\MSSQLSERVER2019";
             conBuilder.InitialCatalog = "3D_Drucker";
             conBuilder.IntegratedSecurity = true;
+            con.ConnectionString = conBuilder.ConnectionString;
 
-            selectPrinters();
+            if (!IsPostBack)
+            {
+                gvBindStudents();
+            }
         }
-        private void selectPrinters()
+
+        protected void gvBindStudents()
         {
             DataTable tblPrinters = new DataTable();
 
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = conBuilder.ConnectionString;
             con.Open();
 
             SqlCommand cmd = new SqlCommand("spSelectStudents", con);
@@ -36,11 +40,75 @@ namespace Printer_Reservation_System
             SqlDataAdapter dap = new SqlDataAdapter(cmd);
 
             dap.Fill(tblPrinters);
-
-            gridviewStudents.DataSource = tblPrinters;
-            gridviewStudents.DataBind();
-
             con.Close();
+            if (tblPrinters.Rows.Count > 0)
+            {
+                gridviewStudents.DataSource = tblPrinters;
+                gridviewStudents.DataBind();
+            }
+            else
+            {
+                tblPrinters.Rows.Add(tblPrinters.NewRow());
+                gridviewStudents.DataSource = tblPrinters;
+                gridviewStudents.DataBind();
+                int columncount = gridviewStudents.Rows[0].Cells.Count;
+                gridviewStudents.Rows[0].Cells.Clear();
+                gridviewStudents.Rows[0].Cells.Add(new TableCell());
+                gridviewStudents.Rows[0].Cells[0].ColumnSpan = columncount;
+                gridviewStudents.Rows[0].Cells[0].Text = "No Records Found";
+            }
+        }
+
+        protected void gridviewStudents_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)gridviewStudents.Rows[e.RowIndex];
+            Label lbldeleteid = (Label)row.FindControl("lblID");
+            Console.WriteLine(lbldeleteid);
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand("delete FROM detail where id='" + Convert.ToInt32(gridviewStudents.DataKeys[e.RowIndex].Value.ToString()) + "'", con);
+            //cmd.ExecuteNonQuery();
+            //con.Close();
+            //gvBindStudents();
+        }
+
+        protected void gridviewStudents_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gridviewStudents.EditIndex = e.NewEditIndex;
+            gvBindStudents();
+        }
+        protected void gridviewStudents_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userid = Convert.ToInt32(gridviewStudents.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = (GridViewRow)gridviewStudents.Rows[e.RowIndex];
+            Label lblID = (Label)row.FindControl("lblID");
+            //TextBox txtname=(TextBox)gr.cell[].control[];  
+            TextBox textName = (TextBox)row.Cells[0].Controls[0];
+            TextBox textadd = (TextBox)row.Cells[1].Controls[0];
+            TextBox textc = (TextBox)row.Cells[2].Controls[0];
+            //TextBox textadd = (TextBox)row.FindControl("txtadd");  
+            //TextBox textc = (TextBox)row.FindControl("txtc");  
+            gridviewStudents.EditIndex = -1;
+            con.Open();
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", con);  
+            SqlCommand cmd = new SqlCommand("update detail set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            gvBindStudents();
+            //gridviewStudents.DataBind();  
+        }
+
+
+
+        protected void gridviewStudents_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridviewStudents.PageIndex = e.NewPageIndex;
+            gvBindStudents();
+        }
+
+        protected void gridviewStudents_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gridviewStudents.EditIndex = -1;
+            gvBindStudents();
         }
     }
 }
